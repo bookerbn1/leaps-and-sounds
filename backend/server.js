ef7bd8240e88f0d69d527dab138d0a85331363f7
Benjamin Booker <booker.benjamin1@gmail.com>
2022-12-08 21:13:30 -0500

    leaps-and-sounds

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require("path");
+const dotenv = require("dotenv");
 
-require('dotenv').config();
+dotenv.config({path: path.resolve(__dirname, '../backend/.env') });;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,6 +19,7 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
+mongoose.set('useFindAndModify', false)
 
 const sessionsRouter = require('./routes/sessions');
 const therapistsRouter = require('./routes/therapists');
@@ -27,7 +30,13 @@ app.use('/sessions', sessionsRouter);
 app.use('/therapists', therapistsRouter);
 app.use('/clients', clientsRouter);
 
+// server prodcution assets
+if(process.env.NODE_ENV === "production")
+app.use(express.static(path.join("../build")))
+app.get("*", (req, res) => res.sendfile(path.resolve(__dirname, 'build', 'index.html')))
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
+//https://github.com/shrutiicodes/mern-practices/blob/master/package.json https://www.youtube.com/watch?v=_5wEqHZJRu4 
